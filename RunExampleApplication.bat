0bb02081a9e4d5fd6bbbb90b956294964e63a891
Dave Saunders <davesaunders36@gmail.com>
2017-11-21 13:14:45 +0000

    Example batch file

---
+++ RunExampleApplication.bat
@@ -0,0 +1,5 @@
+set COR_PROFILER={DF9EDC4B-25C1-4925-A3FB-6AAEB3E2FACD}
+set COR_PROFILER_PATH=%~dp0Src\ExampleProfiler\x64\Debug\ExampleProfiler.dll
+set COR_ENABLE_PROFILING=1
+
+%~dp0Src\TestApplication\bin\Debug\TestApplication.exe
